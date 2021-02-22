2977ce9d78b096f5d850c175f85c6086015aad90
56tiagra <gli0471@hotmail.com>
2021-02-22 21:30:10 +1100

    finalize functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { cloneDeep } from 'lodash'
-import {calculateRotation, Direction, FACING, Position, ROTATE_DIRECTION} from './utils/robotEngine'
+import {calculateRotation, convertFacingBasedOnRoation, Direction, FACING, move, Position, rotate, ROTATE_DIRECTION} from './utils/robotEngine'
 
 function App() {
   const [position, setposition] = useState<Position>({
@@ -13,6 +13,8 @@ function App() {
     rotate: 0,
     facing: FACING.NORTH as Direction
   });
+
+  const [output, setoutput] = useState<string[]>([])
   const xCoordinate = useRef<any>(null)
   const yCoordinate = useRef<any>(null)
   const facing = useRef<any>(null)
@@ -29,42 +31,6 @@ function App() {
     setposition(newCoordinate);
   }
 
-  const convertFacingBasedOnRoation = (degree: number): Direction => {
-    if(degree%90!=0) {
-      return FACING.NORTH as Direction;
-    }
-    switch (degree) {
-      case 0:
-        return FACING.NORTH as Direction;
-      case 90:
-        return FACING.EAST as Direction;
-      case 180:
-        return FACING.SOUTH as Direction;
-      case 270:
-        return FACING.WEST as Direction
-    }
-    return FACING.NORTH as Direction;
-  }
-
-  const rotate = (currentDegree: number, direction: string): number => {
-    if (currentDegree < 0 || currentDegree > 270 || currentDegree % 90 !== 0) {
-      //default to 0 if not validate
-      return 0
-    }
-    if (direction === ROTATE_DIRECTION.LEFT) {
-      if (currentDegree == 0) {
-        return 270;
-      }
-      return currentDegree - 90;
-    }
-    else if (direction === ROTATE_DIRECTION.RIGHT) {
-      if (currentDegree === 270) {
-        return 0;
-      }
-      return currentDegree + 90
-    }
-    return currentDegree;
-  }
 
   const onRotationClick = (direction: string) => {
       const newRotation = rotate(position.rotate, direction)
@@ -74,22 +40,7 @@ function App() {
       setposition(positionCopy)
   }
 
-  const move = (position: Position) => {
-    let positionCopy = cloneDeep(position);
-    if(position.facing == FACING.NORTH || position.coordinate.y < 4) {
-      positionCopy.coordinate.y = positionCopy.coordinate.y + 1;
-    }
-    else if(position.facing == FACING.SOUTH || position.coordinate.y > 1) {
-      positionCopy.coordinate.y--;
-    }
-    else if(position.facing == FACING.EAST || position.coordinate.x < 4) {
-      positionCopy.coordinate.x++;
-    }
-    else if(position.facing == FACING.WEST || position.coordinate.x > 1) {
-      positionCopy.coordinate.x--;
-    }
-    return positionCopy;
-  }
+
 
   const onMoveClick = () => {
     const newPosition = move(position);
@@ -133,11 +84,13 @@ function App() {
           <button className="btn"  onClick={()=> onRotationClick(ROTATE_DIRECTION.RIGHT)
           }>Rotate Right</button>
           <button className="btn" onClick={onMoveClick}>Move</button>
-          <button className="btn">Report</button>
+          <button className="btn" onClick={()=>{setoutput([`${position.coordinate.x},${position.coordinate.y},${position.facing}`,...output])}}>Report</button>
         </div>
         <div className="row">
           <div className="robot__controls__display">
-            <p>test output</p>
+            {output.map((value, index)=> <p>
+              {value}
+            </p>)}
           </div>
         </div>
 
--- src/utils/robotEngine.ts
+++ src/utils/robotEngine.ts
@@ -1,3 +1,5 @@
+import { cloneDeep } from "lodash";
+
 export type Direction = 'NORTH' | 'SOUTH' | 'EAST' | 'WEST';
 
 export type Position = {
@@ -35,3 +37,56 @@ export const calculateRotation = (facing: Direction): number => {
   }
 }
 
+export const move = (position: Position) => {
+  let positionCopy = cloneDeep(position);
+  if(position.facing == FACING.NORTH && position.coordinate.y > 0) {
+    positionCopy.coordinate.y--;
+  }
+  else if(position.facing == FACING.SOUTH && position.coordinate.y < 4) {
+    positionCopy.coordinate.y++;
+  }
+  else if(position.facing == FACING.EAST && position.coordinate.x < 4) {
+    positionCopy.coordinate.x++;
+  }
+  else if(position.facing == FACING.WEST && position.coordinate.x > 0) {
+    positionCopy.coordinate.x--;
+  }
+  return positionCopy;
+}
+
+export const convertFacingBasedOnRoation = (degree: number): Direction => {
+  if(degree%90!=0) {
+    return FACING.NORTH as Direction;
+  }
+  switch (degree) {
+    case 0:
+      return FACING.NORTH as Direction;
+    case 90:
+      return FACING.EAST as Direction;
+    case 180:
+      return FACING.SOUTH as Direction;
+    case 270:
+      return FACING.WEST as Direction
+  }
+  return FACING.NORTH as Direction;
+}
+
+export const rotate = (currentDegree: number, direction: string): number => {
+  if (currentDegree < 0 || currentDegree > 270 || currentDegree % 90 !== 0) {
+    //default to 0 if not validate
+    return 0
+  }
+  if (direction === ROTATE_DIRECTION.LEFT) {
+    if (currentDegree == 0) {
+      return 270;
+    }
+    return currentDegree - 90;
+  }
+  else if (direction === ROTATE_DIRECTION.RIGHT) {
+    if (currentDegree === 270) {
+      return 0;
+    }
+    return currentDegree + 90
+  }
+  return currentDegree;
+}
